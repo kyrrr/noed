5a7b41a7733de328ff63dd954c480576cf43d2c1
kyrrr <kyrrebugge@gmail.com>
2017-05-13 19:12:05 +0200

    git add twep/settings.py git add twep/settings.py

--- twep/settings.py
+++ twep/settings.py
@@ -1,9 +1,9 @@
 API_KEYS = {
         'TWITTER': {
-            'CONSUMER_KEY': '8C8E7EhZQAXS6YJ2eeu2NSSmb',
-            'CONSUMER_SECRET': 'd4L7DGxHbZDlTm39E2UvWqrhZ8GbqnXTyMUrVo1ygxzruyi6fW',
-            'ACCESS_TOKEN': '55298054-VLQUlpDOX7vM1PT6XDGFg8ai4A71ltAONU1dM9YV2',
-            'ACCESS_SECRET': 'dJwD9Pw1gLA1biMSbHZASgmn0NoUjTd0ivK3Q1G8q83p4'
+            'CONSUMER_KEY': '!!!!',
+            'CONSUMER_SECRET': '!!!!',
+            'ACCESS_TOKEN': '!!!!',
+            'ACCESS_SECRET': '!!!!'
         }
     }
 
